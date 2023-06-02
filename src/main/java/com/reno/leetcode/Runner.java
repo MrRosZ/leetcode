0ee635e23d94f3081996fb64429fee5ddbb9fb2e
Rosardo Zarbin <RosardoZarbin@earth.com>
2023-06-02 09:24:27 -0400

    Leetcode 88 MergeSortedArray

--- src/main/java/com/reno/leetcode/Runner.java
+++ src/main/java/com/reno/leetcode/Runner.java
@@ -2,12 +2,10 @@ package com.reno.leetcode;
 
 import com.reno.leetcode.array.MergeSortedArray;
 
-import java.util.Arrays;
-
 public class Runner {
     public static void main(String[] args) {
         MergeSortedArray mergeSortedArray = new MergeSortedArray();
-        mergeSortedArray.merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6}, 3);
+        mergeSortedArray.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
         mergeSortedArray.merge(new int[]{1}, 1, new int[]{}, 0);
         mergeSortedArray.merge(new int[]{0}, 0, new int[]{1}, 1);
 
