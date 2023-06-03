20edd8d3a8bc8dcfe6c4c2188c07f89a59296aa9
Rosardo Zarbin <RosardoZarbin@earth.com>
2023-06-02 20:26:42 -0400

    Leetcode 27 RemoveElement

--- src/main/java/com/reno/leetcode/Runner.java
+++ src/main/java/com/reno/leetcode/Runner.java
@@ -1,13 +1,18 @@
 package com.reno.leetcode;
 
 import com.reno.leetcode.array.MergeSortedArray;
+import com.reno.leetcode.array.RemoveElement;
 
 public class Runner {
     public static void main(String[] args) {
-        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        /*MergeSortedArray mergeSortedArray = new MergeSortedArray();
         mergeSortedArray.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
         mergeSortedArray.merge(new int[]{1}, 1, new int[]{}, 0);
-        mergeSortedArray.merge(new int[]{0}, 0, new int[]{1}, 1);
+        mergeSortedArray.merge(new int[]{0}, 0, new int[]{1}, 1);*/
+        RemoveElement removeElement = new RemoveElement();
+        removeElement.removeElement(new int[]{0,1,2,2,3,0,4,2}, 2);
+        removeElement.removeElement(new int[]{3,2,2,3}, 3);
+
 
     }
 }
